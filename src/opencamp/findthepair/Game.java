285f4e289e7fc23afcd53ae2206e2b0af4e4e6f9
Hanjong Ko <rino0601@naver.com>
2014-04-27 01:42:35 +0900

    implement prepare sequance!

--- src/opencamp/findthepair/Game.java
+++ src/opencamp/findthepair/Game.java
@@ -9,8 +9,10 @@ import opencamp.findthepair.model.Card;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -18,6 +20,9 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Game extends Activity {
 
@@ -26,19 +31,19 @@ public class Game extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.game);
 		
+		// init gridView
 		GridView gridview = (GridView) findViewById(R.id.gridview);
 		IImageAdapter iImageAdapter = new IImageAdapter(this);
 		gridview.setOnItemClickListener(iImageAdapter);
-		
+		// load resource.
 		TypedArray ta = getResources().obtainTypedArray(R.array.cards);
-		
 		int length = ta.length();
 		ArrayList<Integer> resId = new ArrayList<Integer>(length);
 		for (int i=0;i<length;i++) {
 			resId.add(ta.getResourceId(i, 0));
 		}		
 		ta.recycle();
-		
+		// add resource into adapter properly.
 		Collections.shuffle(resId);		
 		for(int i : resId) {
 			iImageAdapter.add(new Card(i));
@@ -47,8 +52,98 @@ public class Game extends Activity {
 		for(int i : resId) {
 			iImageAdapter.add(new Card(i));
 		}
-		
+		// finish init.
 		gridview.setAdapter(iImageAdapter);
+		
+		// init timer
+		ProgressBar progressBarTime = (ProgressBar) findViewById(R.id.progressBarTime);
+		
+		//init timer Text
+		TextView textViewTime = (TextView) findViewById(R.id.textViewTime);
+		
+		new PreSequanceTasker(gridview, progressBarTime, textViewTime).execute();
+	}
+	
+	public static class PreSequanceTasker extends AsyncTask<Void, Integer, Void> {
+		private GridView gdv;
+		private ProgressBar pbar;
+		private TextView text;
+		
+		private int timeLmt = 10;
+		private int tick;
+		private int max;
+		
+		public PreSequanceTasker(GridView gdv, ProgressBar pbar, TextView text) {
+			this.gdv = gdv;
+			this.pbar = pbar;
+			this.text = text;
+			
+			tick = 1000/30;
+			max = timeLmt*tick*30;
+		}
+		
+		
+		@Override
+		protected void onPreExecute() {
+			pbar.setMax(max);
+			pbar.setProgress(max);
+			
+			text.setText(timeLmt+" Sec");
+			
+			IImageAdapter adapter = (IImageAdapter) gdv.getAdapter();
+			int count = adapter.getCount();
+			for(int i=0; i < count; i++) {
+				Card item = (Card) adapter.getItem(i);
+				item.touch();
+			}
+			adapter.notifyDataSetChanged();
+			
+			super.onPreExecute();
+		}
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				for(int i=0; i<timeLmt ; i++ ) {
+					for(int j=0;j<30;j++) {
+						Thread.sleep(tick);
+						publishProgress(tick,0);
+					}
+					publishProgress(0,timeLmt-i-1);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			int progress = values[0];
+			int sec = values[1];
+			
+			if(progress!=0) {
+				pbar.incrementProgressBy(-progress);
+			} else {
+				text.setText(sec+" Sec");
+			}
+			super.onProgressUpdate(values);
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			IImageAdapter adapter = (IImageAdapter) gdv.getAdapter();
+			int count = adapter.getCount();
+			for(int i=0; i < count; i++) {
+				Card item = (Card) adapter.getItem(i);
+				item.clear();
+			}
+			adapter.notifyDataSetChanged();
+			
+			super.onPostExecute(result);
+		}
+		
+
 	}
 	
 	public class IImageAdapter extends ArrayAdapter<Card> implements OnItemClickListener{
